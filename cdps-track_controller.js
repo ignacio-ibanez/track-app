9bcdbd29a1b04716f8bd19e8bf642b763275e907
Naomigl <naomi.glanga@gmail.com>
2016-01-16 23:40:44 +0000

    Eliminandos comentarios inutiles y track-model

--- cdps-track_controller.js
+++ cdps-track_controller.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var track_model = require('./cdps-track_model');
 
 // Escribe una nueva canción en el registro de canciones y la guarda en el cluster
 exports.create = function (req, res) {
@@ -8,9 +7,6 @@ exports.create = function (req, res) {
 	var id = track.name.split('.')[0];
 	console.log('id guardado en listener: ', id);
 	var name = track.originalname.split('.')[0];
-	
-	// TODO: cambiar url
-	var url = '/mnt/nas/' + track.originalname;
 	var path = '/mnt/nas/'+name;
 	
 	// Guardamos la cancion en el cluster
@@ -21,31 +17,17 @@ exports.create = function (req, res) {
 	    console.log("Audio guardado.");
 	}); 
 
-	// Escribe los metadatos de la nueva canción en el registro.
-/*	track_model.tracks[name] = {
-		name: name,
-		url: url
-	};
-*/	
 	res.status(200).send('Archivo subido.');
 };
 
 // Borra una canción (trackId) del registro de canciones 
-// TODO:
-// - Eliminar en tracks.cdpsfy.es el fichero de audio correspondiente a trackId
 exports.destroy = function (req, res) {
-	var id = req.params.trackId;
-	
+	var id = req.params.trackId;	
 	console.log("Borrando: "+id);
-	// Aquí debe implementarse el borrado del fichero de audio indetificado por trackId en tracks.cdpsfy.es
-//	var track = track_model.tracks[id];
-	//var name = track.name + '.mp3';
-//	console.log('Path a borrar: ', track.url);
+
+	// Borramos el archivo
 	fs.unlinkSync('/mnt/nas/'+id);
 
-	// Borra la entrada del registro de datos
-//	delete track_model.tracks[id];
-	
 	console.log('Exito borrando archivo');
 	res.status(200).send('Archivo eliminado: '+id);
 };
